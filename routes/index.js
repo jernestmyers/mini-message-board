9d3ddb459d1f06f37cc6d7682f23a7a82407ca3b
Jeremy Myers <inernestmusic@gmail.com>
2022-01-03 18:54:57 -0500

    Create form to add messages

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.messageContent,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = router;
